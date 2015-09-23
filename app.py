649263d28dcdcc7ead60a43d11e7d20da9267be4
Mo Ben <mo.ben@youvisit.com>
2015-09-23 14:15:24 -0400

    first changes

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
+import matplotlib.pyplot  as plot
+
+import numpy              as np
+
 from sample import sample as X
 
+sampleArray = np.array(X)
+
+screenValues = np.zeros((len(X),len(X[0]), 3), order="F")
+
+for x in range(width["view"]):
+  for y in range(height):
+    value = sampleArray[x][y]
+    screenValues[x][y] = np.array([value,value,value])
+    # screenInt[x][y] *= 126
+
+plot.imshow(X,interpolation="nearest")
+plot.show()
