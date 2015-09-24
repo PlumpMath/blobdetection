8e719c7e1455e5bf7d05309c162b9652ddee02aa
William Stein <williamstein92@gmail.com>
2015-09-24 11:36:03 -0400

    yoyoyoyoy

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from matplotlib import pyplot as plt
 
 np.set_printoptions(threshold=np.nan)
 
-N = 0
-
 max_blob = 0
 
 Y = np.zeros(X.shape)
@@ -15,6 +13,9 @@ w, h = X.shape
 
 blobs = np.zeros(h * w)
 blob_index = 0
+
+def clone():
+    return np.zeros(X.shape)
 
 def contiguous_left(x, y):
     return X[x][y] == X[x - 1][y]
@@ -28,20 +29,27 @@ def contiguous_up(x, y):
 def contiguous_down(x, y):
     return X[x][y] == X[x][y + 1]
 
-def merge_shapes(x, y, i):
+def merge_shapes(x, y, i, V):
     if Y[x][y] != -1:
         Y[x][y] = i
+        V[x][y] = 1.0
 
-        if contiguous_left(x, y):
-            merge_shapes(x - 1, y, i)
-            # N += 1
-        if contiguous_up(x, y):
-            merge_shapes(x, y - 1, i)
-            # N += 1
+        if x < w - 1 and contiguous_right(x, y) and V[x + 1][y] != 1.0:
+            merge_shapes(x + 1, y, i, V)
+
+        if x > 0 and contiguous_left(x, y) and V[x - 1][y] != 1.0:
+            merge_shapes(x - 1, y, i, V)
+
+        if y < h - 1 and contiguous_down(x, y) and V[x][y + 1] != 1.0:
+            merge_shapes(x, y + 1, i, V)
+
+        if y > 0 and contiguous_up(x, y) and V[x][y - 1] != 1.0:
+            merge_shapes(x, y - 1, i, V)
 
 for x in range(w):
     for y in range(h):
-        # N += 1
+        right = False
+        down = False
         if Y[x][y] == -1:
             Y[x][y] = blob_index
             blobs[blob_index] += 1
@@ -70,34 +78,44 @@ for x in range(w):
         # else:
         #     blob_index += 1
 
+        # backwards.. this goes down
+        if x < w - 1 and contiguous_right(x,y):
+            if Y[x+1][y] != -1:
+                blobs[current_index] += blobs[Y[x+1][y]]
+                blobs[Y[x+1][y]] = 0
+                merge_shapes(x+1,y,current_index, clone())
+            else:
+                Y[x+1][y] = current_index
+                blobs[current_index] += 1
+                right = True
+
         # backwards.. this goes right
         if y < h - 1 and contiguous_down(x,y):
-
             # there's already a shape!
             if Y[x][y+1] != -1:
                 blobs[current_index] += blobs[Y[x][y+1]]
                 blobs[Y[x][y+1]] = 0
-                merge_shapes(x,y+1,current_index)
+                merge_shapes(x,y+1,current_index, clone())
             else:
                 Y[x][y+1] = current_index
                 blobs[current_index] += 1
+                down = True
         else:
             blob_index += 1
 
-        # backwards.. this goes down
-        if x < w - 1 and contiguous_right(x,y):
-            Y[x + 1][y] = current_index
-            blobs[current_index] += 1
-
         if blobs[current_index] > max_blob:
             max_blob = blobs[current_index]
+            Z[x][y] = 1.0
+            if right:
+                Z[x+1][y] = 1.0
+            if down:
+                Z[x][y+1] = 1.0
 
-print N
 print max_blob
 
 plt.figure(1)
-plt.subplot(211)
+plt.subplot(221)
 plt.imshow(X, interpolation="nearest")
-plt.subplot(212)
-plt.imshow(Y, interpolation="nearest")
+plt.subplot(222)
+plt.imshow(Y, interpolation="nearest")\
 plt.show()
